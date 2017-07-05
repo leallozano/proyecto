40ae318d756eb7a39137c01049b67474283dbef4
Carlos <leal.lozano@gmail.com>
2017-07-05 12:34:36 -0400

    proyecto retrofit

--- app/src/main/java/com/example/carlos/proyecto/MainActivity.java
+++ app/src/main/java/com/example/carlos/proyecto/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    // TODO - insert your themoviedb.org API KEY here
+
     private final static String API_KEY = "b5297900952d5edc9daf9f34cd717a8a";
 
     @Override
@@ -48,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void ejecutar_class_inf(View v){
 
-        //  Intent intenInf = new Intent(this,Inflate_Menu.class);
-        //  startActivity(intenInf);
+
     }
 
     @Override
@@ -139,31 +138,7 @@ public class MainActivity extends AppCompatActivity {
             });
 
 
-        }/*else if (getType == 4) {
-            Call<MovieResponse> call = apiService.getMovieDetails (297762,API_KEY);
-            call.enqueue(new Callback<MovieResponse>() {
-                @Override
-                public void onResponse(Call<MovieResponse>call, Response<MovieResponse> response) {
-                    List<Movie> movies = response.body().getResults();
-                    Log.d(TAG, "Number of movies received: " + movies.size());
-                    Toast.makeText(MainActivity.this, "Number of movies received: " + movies.size(), Toast.LENGTH_LONG).show();
-                    recyclerView.setAdapter(new Adapter(movies, R.layout.list_item_movie, getApplicationContext()));
-                }
-
-                @Override
-                public void onFailure(Call<MovieResponse>call, Throwable t) {
-                    // Log error here since request failed
-                    Log.e(TAG, t.toString());
-                }
-            });
-        }*/
-
-
-    /*    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }*/
+        }
 }
 
 
